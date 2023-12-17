887cdca3c7e72e9c277d8465bc01fbe0815bf188
galaxy <1045208237@qq.com>
2023-12-17 21:02:12 +0800

    hadoop

---
+++ se3353_24_hadoopmrsample/src/main/java/WordCount.java
@@ -0,0 +1,141 @@
+//import java.io.IOException;
+//import java.util.StringTokenizer;
+//
+//import org.apache.hadoop.conf.Configuration;
+//import org.apache.hadoop.fs.Path;
+//import org.apache.hadoop.io.IntWritable;
+//import org.apache.hadoop.io.Text;
+//import org.apache.hadoop.mapreduce.Job;
+//import org.apache.hadoop.mapreduce.Mapper;
+//import org.apache.hadoop.mapreduce.Reducer;
+//import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+//import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+//
+//public class WordCount {
+//
+//    public static class TokenizerMapper
+//            extends Mapper<Object, Text, Text, IntWritable>{
+//
+//        private final static IntWritable one = new IntWritable(1);
+//        private Text word = new Text();
+//
+//        public void map(Object key, Text value, Context context
+//        ) throws IOException, InterruptedException {
+//            StringTokenizer itr = new StringTokenizer(value.toString());
+//            while (itr.hasMoreTokens()) {
+//                word.set(itr.nextToken());
+//                context.write(word, one);
+//            }
+//        }
+//    }
+//
+//    public static class IntSumReducer
+//            extends Reducer<Text,IntWritable,Text,IntWritable> {
+//        private IntWritable result = new IntWritable();
+//
+//        public void reduce(Text key, Iterable<IntWritable> values,
+//                           Context context
+//        ) throws IOException, InterruptedException {
+//            int sum = 0;
+//            for (IntWritable val : values) {
+//                sum += val.get();
+//            }
+//            result.set(sum);
+//            context.write(key, result);
+//        }
+//    }
+//
+//    public static void main(String[] args) throws Exception {
+//        Configuration conf = new Configuration();
+//        conf.set("dfs.defaultFS", "hdfs://hadoop:9000");
+//        conf.set("mapreduce.cluster.local.dir","/Users/simmucheng/tmp/");
+//        Job job = Job.getInstance(conf, "word count");
+//        job.setJarByClass(WordCount.class);
+//        job.setMapperClass(TokenizerMapper.class);
+//        job.setCombinerClass(IntSumReducer.class);
+//        job.setReducerClass(IntSumReducer.class);
+//        job.setOutputKeyClass(Text.class);
+//        job.setOutputValueClass(IntWritable.class);
+//        FileInputFormat.addInputPath(job, new Path(args[0]));
+//        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+//        System.exit(job.waitForCompletion(true) ? 0 : 1);
+//    }
+//}
+
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+
+import java.io.IOException;
+import java.util.StringTokenizer;
+
+public class WordCount {
+
+    public static class TokenizerMapper
+            extends Mapper<Object, Text, Text, IntWritable> {
+
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        private final static String[] keywords = {"classic", "fiction", "science","adventure"};
+
+        public void map(Object key, Text value, Context context
+        ) throws IOException, InterruptedException {
+            StringTokenizer itr = new StringTokenizer(value.toString());
+            while (itr.hasMoreTokens()) {
+                String token = itr.nextToken().toLowerCase(); // Convert to lowercase for case-insensitive comparison
+                for (String keyword : keywords) {
+                    if (token.contains(keyword)) {
+                        word.set(keyword);
+                        context.write(word, one);
+                    }
+                }
+            }
+        }
+    }
+
+    public static class IntSumReducer
+            extends Reducer<Text,IntWritable,Text,IntWritable> {
+        private IntWritable result = new IntWritable();
+
+        public void reduce(Text key, Iterable<IntWritable> values,
+                           Context context
+        ) throws IOException, InterruptedException {
+            int sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
+            }
+            result.set(sum);
+            context.write(key, result);
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        Configuration conf = new Configuration();
+        conf.set("dfs.defaultFS", "hdfs://hadoop:9000");
+        conf.set("mapreduce.cluster.local.dir","/Users/simmucheng/tmp/");
+        Job job = Job.getInstance(conf, "word count");
+        job.setJarByClass(WordCount.class);
+        job.setMapperClass(TokenizerMapper.class);
+        job.setCombinerClass(IntSumReducer.class);
+        job.setReducerClass(IntSumReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+// 获取Mapper的数量
+        int numMapTasks = job.getConfiguration().getInt("mapreduce.job.maps", 1);
+        System.out.println("Mapper数量：" + numMapTasks);
+// 获取Reducer的数量
+        int numReduceTasks = job.getConfiguration().getInt("mapreduce.job.reduces", 1);
+        System.out.println("Reducer数量：" + numReduceTasks);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
+    }
+}
